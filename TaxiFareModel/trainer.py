608db6be35bcc955d72123944b514418e7d0446a
Eloisa Hernandez <eloisa.hdez.r@gmail.com>
2021-11-16 14:57:07 +0100

    Integrated with MLFlow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,8 +8,16 @@ from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.data import get_data, clean_data
 
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+
 
 class Trainer():
+
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    experiment_name = "[DE] [Munich] [eloisahernandez] TaxFareModel"
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -33,6 +41,7 @@ class Trainer():
                                          remainder="drop")
         self.pipeline = Pipeline([('preproc', preproc_pipe),
                          ('linear_model', LinearRegression())])
+        self.mlflow_log_param('model', 'LinearRegression')
 
 
     def run(self):
@@ -45,7 +54,31 @@ class Trainer():
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric('rmse', rmse)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
